1cc81926e02c6f24b0fa25e9c338a70f31a6afc1
KangMinjeong <a50070863@gmail.com>
2023-01-05 22:29:46 +0900

    Feat: 추가 채팅 입장, 대화 기능

---
+++ src/main/java/com/example/chatting/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.example.chatting.controller;
+
+import com.example.chatting.domain.ChatRoom;
+import com.example.chatting.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+public class ChatController {
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestBody String name){
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom(){
+        return chatService.findAllRoom();
+    }
+}
